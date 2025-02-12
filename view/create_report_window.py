bdc11ee4c45289b2e10e27a5c376490415dc40fc
egorttimofeev <nasuschnov487@gmail.com>
2025-02-12 17:41:30 +0300

    создал окно

---
+++ view/create_report_window.py
@@ -0,0 +1,23 @@
+from PyQt6 import QtCore, QtGui
+from PyQt6.QtWidgets import QDialog, QWidget, QScrollBar, QListWidget, QListWidgetItem, QTabWidget, QApplication
+from service.client_manager import ClientManager
+from service.user_manager import UserManager
+from view.create_report_w_controller import *
+
+class CreateReportWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui_create_report_window()
+
+    def ui_create_report_window(self):
+        self.setWindowTitle("Создать отчет")
+        self.setFixedSize(568, 319)
+
+        
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    form = CreateReportWindow()
+    form.show()
+    sys.exit(app.exec())
